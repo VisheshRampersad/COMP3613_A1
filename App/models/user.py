2828e926212e006aafbf8ef9d7f80cb894f365fd
VisheshRampersad <visheshrampersad2005@gmail.com>
2025-09-29 22:00:23 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     role = db.Column(db.String(20), nullable=False, default='staff')
-    #is_active = db.Column(db.Boolean, nullable =False, default=True)
+    
     
     shifts = db.relationship('Shift', backref='user', lazy=True)
 
